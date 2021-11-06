a559fdf0ce5a20a068ebe3888049e860302b3689
Anya Ch <annnacha@yandex.ru>
2021-11-06 22:48:06 +0300

    updating README, updating port number

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const NotFoundError = require('./errors/not-found-err');
 const errorHandler = require('./errors/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 
