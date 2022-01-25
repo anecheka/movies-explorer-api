06bd91e779cee2fb8079f7fd33498e097427d9ac
Anya Ch <annnacha@yandex.ru>
2022-01-25 22:17:23 +0300

    feat: allowed cors added

--- app.js
+++ app.js
@@ -13,13 +13,46 @@ const app = express();
 
 app.use(cookieParser());
 
-mongoose.connect(DATABASE);
+mongoose.connect(DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
 app.use(requestLogger);
 
 app.use(limiter);
+
+const allowedCors = [
+  'https://cinematheque.nomoredomains.work',
+  'http://cinematheque.nomoredomains.work',
+  'localhost:3000',
+  'http://localhost:3000',
+];
+
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
+});
 
 app.use('/', router);
 
